0d61dca5a049615f1d546712254522d3c56275c8
성현준 <sunghj1118@gmail.com>
2023-05-25 14:34:20 +0900

    All files up to Section 7.

---
+++ src/main/java/calculator/server/CalculatorServiceImpl.java
@@ -0,0 +1,60 @@
+package calculator.server;
+
+import com.proto.calculator.*;
+import io.grpc.stub.StreamObserver;
+
+import java.util.stream.Stream;
+
+public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
+
+    @Override
+    public void sum(SumRequest request, StreamObserver<SumResponse> responseObserver) {
+        responseObserver.onNext(SumResponse.newBuilder().setResult(
+                request.getFirstNumber() + request.getSecondNumber()
+        ).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void primes(PrimeRequest request, StreamObserver<PrimeResponse> responseObserver) {
+        int number = request.getFirstNumber();
+        int divisor = 2;
+
+        while (number > 1) {
+            if (number % divisor == 0) {
+                number = number / divisor;
+                responseObserver.onNext(PrimeResponse.newBuilder().setPrimeFactor(divisor).build());
+            } else {
+                ++divisor;
+            }
+        }
+
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<AvgRequest> avg(StreamObserver<AvgResponse> responseObserver) {
+        return new StreamObserver<AvgRequest>() {
+            int sum = 0;
+            int count = 0;
+
+            @Override
+            public void onNext(AvgRequest value) {
+                sum += value.getNumber();
+                ++count;
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                responseObserver.onError(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(AvgResponse.newBuilder().setResult(
+                        (double)sum / count
+                ).build());
+            }
+        };
+    }
+}
---
+++ src/main/java/greeting/client/GreetingClient.java
@@ -0,0 +1,90 @@
+package greeting.client;
+
+import com.proto.greeting.GreetingRequest;
+import com.proto.greeting.GreetingResponse;
+import com.proto.greeting.GreetingServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class GreetingClient {
+
+    private static void doGreet(ManagedChannel channel){
+        System.out.println("Enter doGreet");
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+        GreetingResponse response = stub.greet(GreetingRequest.newBuilder().setFirstName("Hyunjoon").build());
+
+        System.out.println("Greeting: " + response.getResult());
+    }
+
+    private static void doGreetManyTimes(ManagedChannel channel) {
+        System.out.println("Enter doGreetManyTimes");
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+
+        stub.greetManyTimes(GreetingRequest.newBuilder().setFirstName("Hyunjoon").build()).forEachRemaining(response -> {
+            System.out.println(response.getResult());
+        });
+    }
+
+    private static void doLongGreet(ManagedChannel channel) throws InterruptedException {
+        System.out.println("Enter doLongGreet");
+        GreetingServiceGrpc.GreetingServiceStub stub = GreetingServiceGrpc.newStub(channel);
+
+        List<String> names = new ArrayList<>();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Collections.addAll(names, "Hyunjoon", "Junghyun", "Hajin");
+
+        StreamObserver<GreetingRequest> stream = stub.longGreet(new StreamObserver<GreetingResponse>() {
+            @Override
+            public void onNext(GreetingResponse response) {
+                System.out.println(response.getResult());
+            }
+
+            @Override
+            public void onError(Throwable t) {}
+
+            @Override
+            public void onCompleted() {
+                latch.countDown();
+            }
+        });
+
+        for (String name : names) {
+            stream.onNext(GreetingRequest.newBuilder().setFirstName(name).build());
+        }
+
+        stream.onCompleted();
+        latch.await(3, TimeUnit.SECONDS);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length == 0){
+            System.out.println("Need one argument to work");
+        }
+
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+        switch (args[0]){
+            case "greet": doGreet(channel); break;
+            case "greet_many_times": doGreetManyTimes(channel); break;
+            case "long_greet": doLongGreet(channel); break;
+            default:
+                System.out.println("Keyword Invalid: " + args[0]);
+        }
+
+        // do something
+        System.out.println("Shutting down");
+        channel.shutdown();
+    }
+}
