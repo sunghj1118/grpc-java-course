72f3f417a3e042df8caa42b4416f8cc6314e61c2
성현준 <sunghj1118@gmail.com>
2023-06-02 18:07:54 +0900

    Implemented Deadlines through a Greeting API by checking status and setting time limits

--- src/main/java/greeting/client/GreetingClient.java
+++ src/main/java/greeting/client/GreetingClient.java
@@ -3,8 +3,7 @@ package greeting.client;
 import com.proto.greeting.GreetingRequest;
 import com.proto.greeting.GreetingResponse;
 import com.proto.greeting.GreetingServiceGrpc;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.*;
 import io.grpc.stub.StreamObserver;
 import org.checkerframework.checker.units.qual.C;
 
@@ -91,6 +90,30 @@ public class GreetingClient {
         latch.await(3, TimeUnit.SECONDS);
     }
 
+    private static void doGreetWithDeadline(ManagedChannel channel) {
+        System.out.println("Enter doGreetWithDeadline");
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+
+        GreetingResponse response = stub.withDeadline(Deadline.after(3, TimeUnit.SECONDS))
+                .greetWithDeadline(GreetingRequest.newBuilder().setFirstName("Hyunjoon").build());
+
+        System.out.println("Greeting within deadline: " + response.getResult());
+
+        try {
+            stub.withDeadline(Deadline.after(100, TimeUnit.MILLISECONDS))
+                    .greetWithDeadline(GreetingRequest.newBuilder().setFirstName("Hyunjoon").build());
+
+            System.out.println("Greeting deadline exceeded: " +  response.getResult());
+        } catch (StatusRuntimeException e) {
+            if(e.getStatus().getCode() == Status.Code.DEADLINE_EXCEEDED) {
+                System.out.println("Deadline has been exceeded");
+            } else {
+                System.out.println("Got an exception in GreetWithDeadline");
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
         if (args.length == 0){
             System.out.println("Need one argument to work");
@@ -106,6 +129,7 @@ public class GreetingClient {
             case "greet_many_times": doGreetManyTimes(channel); break;
             case "long_greet": doLongGreet(channel); break;
             case "greet_everyone": doGreetEveryone(channel); break;
+            case "greet_with_deadline": doGreetWithDeadline(channel); break;
             default:
                 System.out.println("Keyword Invalid: " + args[0]);
         }
